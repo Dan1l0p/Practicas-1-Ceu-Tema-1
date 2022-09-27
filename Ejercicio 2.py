1f76bda9b343f1a0a867d0353ae442346b6fa1c9
Dan1l0p <dalova2003@gmail.com>
2022-09-27 15:16:22 +0200

    Comienzo ejr 3

---
+++ Ejercicio 2.py
@@ -0,0 +1,20 @@
+numero_magico = (12345679)
+
+while True:
+    numero_usuario = input("Dime un número del 1 al 9. ")
+    numero_usuario = int(numero_usuario)
+    if numero_usuario <1:
+        print("Un número del 1 al 9...")
+        continue
+    elif numero_usuario >9:
+        print("Un número del 1 al 9...")
+        continue
+    else:
+        print("Calculando...")
+        break
+
+
+numero_usariomult = numero_usuario * 9
+numero_magico2 = numero_magico * numero_usariomult
+print(numero_magico2)
+print("Cálculo terminado.")
