2a7b0494a3a33b120c3376973d104ca5fbb3f76e
Dan1l0p <dalova2003@gmail.com>
2022-09-29 17:39:34 +0200

    ejercicio 6

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,17 +1,19 @@
 def lista_parimpar(lista):
+    lista_par = []
+    lista_impar = []
 
-
-    for i in range(lista):
+    for i in lista:
         if i % 2 == 0:
             lista_par.append(i)
+            lista_par.sort()
 
         else:
             lista_impar.append(i)
+            lista_impar.sort()
 
     return lista_par, lista_impar
 
-lista_par = []
-lista_impar = []
+
 lista = [6, 5, 2, 1, 7]
-lista.sort()
+listaseparada = lista_parimpar(lista)
 print(lista_parimpar(lista))
