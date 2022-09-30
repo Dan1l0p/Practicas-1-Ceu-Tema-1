c3621f9f40543d5c43d18680774313370586cc58
Dan1l0p <dalova2003@gmail.com>
2022-09-30 23:14:33 +0200

    CAMBIOS VARIOS

---
+++ Ejercicio 5.py
@@ -0,0 +1,13 @@
+import sys
+
+
+while True:
+    numero_a_descomponer = input("Dime un número de máximo 4 dígitos el cual descompondremos en unidades, decenas, centenas y miles. ")
+    numero_a_descomponer = int(numero_a_descomponer)
+    if numero_a_descomponer >9999:
+        print("NO es posible. ")
+        continue
+    elif numero_a_descomponer <0:
+        print("NO es posible.  ")
+    else:
+        break
